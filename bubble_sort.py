ed10ae9e28e36cf3d87614f112696de514498ea9
sakshisinghrajput-cyber <sakshi29112004@gmail.com>
2026-03-27 09:36:15 +0530

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+# Example
+arr = [5, 2, 9, 1]
+print("Sorted:", bubble_sort(arr))
